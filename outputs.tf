f1b805bdf436f5b74e5ebebb9e9aa413682623d8
Jim Andreasen <jim.andreasen@reifnir.com>
2021-05-30 20:34:58 -0400

    return useful outputs

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
-output "debug" {
-  value     = azurerm_storage_account.static_site
-  sensitive = true
+output "storage_account_primary_web_endpoint" {
+  description = "The storage account's self-hosted static site URL"
+  value       = azurerm_storage_account.static_site.primary_web_endpoint
+}
+
+output "azure_function_defualt_url" {
+  description = "The storage account's default DNS entry"
+  value       = "https://${azurerm_function_app.static_site.default_hostname}"
 }
