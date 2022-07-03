ff3f5ed5d7d24e52a7dd8289a207abf5db929673
Jim Andreasen <jim@andreasen.dev>
2022-07-03 19:17:35 -0400

    improve readability

--- backend.tf
+++ backend.tf
@@ -7,8 +7,9 @@ terraform {
       version = ">= 3"
     }
     acme = {
+      # Since this isn't a Hashicorp-specific provider, not implicitly trusting patches
       source  = "vancluever/acme"
-      version = "2.9.0" # Since this isn't a Hashicorp-specific provider, not implicitly trusting patches
+      version = "2.9.0"
     }
   }
 }
